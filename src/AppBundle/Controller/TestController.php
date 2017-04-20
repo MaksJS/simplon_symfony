391898fe04d1c995a831385ce10f42f49d8ab823
Maks <maxence.derous@gmail.com>
2017-04-20 13:54:16 +0200

    added test index template

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -2,18 +2,20 @@
 
     namespace AppBundle\Controller;
 
-
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
     use Symfony\Component\HttpFoundation\Request;
-    use Symfony\Component\HttpFoundation\Response;
+    //use Symfony\Component\HttpFoundation\Response;
 
-    class TestController {
+    class TestController extends Controller {
 
         /**
          * @Route("/test", name="test")
          */
         public function indexAction(Request $request) {
-            return new Response("coucou");
+            //return new Response("<html><body><h1>coucou</h1></body></html>");
+            return $this->render('test/index.html.twig', [
+                'username' => 'max'
+            ]);
         }
-
     }
