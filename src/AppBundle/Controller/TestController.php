8117547d387a0e8426c9ec54125373d1581cd29f
Maks <maxence.derous@gmail.com>
2017-04-20 14:19:14 +0200

    added example of route requirements

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -5,17 +5,24 @@
     use Symfony\Bundle\FrameworkBundle\Controller\Controller;
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
     use Symfony\Component\HttpFoundation\Request;
-    //use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Response;
 
     class TestController extends Controller {
 
         /**
-         * @Route("/test", name="test")
+         * @Route("/test/{username}", name="test", requirements={"username": "\D+"})
          */
-        public function indexAction(Request $request) {
+        public function indexAction(Request $request, $username = "max") {
             //return new Response("<html><body><h1>coucou</h1></body></html>");
             return $this->render('test/index.html.twig', [
-                'username' => 'max'
+                'username' => $username
             ]);
+        }
+
+        /**
+         * @Route("/test/{page}", name="test_show", requirements={"page": "\d+"})
+         */
+        public function showAction($page) {
+            return new Response("ma page est ".$page);
         }
     }
