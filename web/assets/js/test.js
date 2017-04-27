e8686a6f27dfe4d66322d3e5cc4550c42fd10670
Maks <maxence.derous@gmail.com>
2017-04-27 15:42:27 +0200

    added ajax example

---
+++ web/assets/js/test.js
@@ -0,0 +1,18 @@
+
+function deleteProduct(id) {
+    $.ajax({
+    url: '/products/' + id + '.json',
+    method: 'DELETE', 
+    }).done(function(data) {
+        console.log(data.notice);
+        $('.panel.panel-success')
+            .show({
+                duration: 2000,
+                complete: function() {
+                    $(this).hide();
+                }
+            });
+        $('.panel.panel-success p')
+            .text(data.notice);
+    });
+}
