601f35de6f9c9d95a2fd41699f289c8368b5a614
Maks <maxence.derous@gmail.com>
2017-04-28 11:49:17 +0200

    added generated setters and getters for Category entity

--- src/AppBundle/Entity/Category.php
+++ src/AppBundle/Entity/Category.php
@@ -21,4 +21,38 @@
          * @ORM\Column(type="string", length=100)
          */
         private $designation;
+    
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
     }
+
+    /**
+     * Set designation
+     *
+     * @param string $designation
+     *
+     * @return Category
+     */
+    public function setDesignation($designation)
+    {
+        $this->designation = $designation;
+
+        return $this;
+    }
+
+    /**
+     * Get designation
+     *
+     * @return string
+     */
+    public function getDesignation()
+    {
+        return $this->designation;
+    }
+}
