6c8a30b56b40ad13495d3ad68ed1bbfe23b6feae
Maks <maxence.derous@gmail.com>
2017-05-04 12:12:40 +0200

    added products attribute to category

--- src/AppBundle/Entity/Category.php
+++ src/AppBundle/Entity/Category.php
@@ -3,7 +3,8 @@
     namespace AppBundle\Entity;
 
     use Doctrine\ORM\Mapping as ORM;
-
+    use Doctrine\Common\Collections\ArrayCollection;
+    
     /**
      * @ORM\Entity
      * @ORM\Table(name="categories")
@@ -21,7 +22,17 @@
          * @ORM\Column(type="string", length=100)
          */
         private $designation;
-    
+        
+        /**
+         * @ORM\OneToMany(targetEntity="Product", mappedBy="category")
+         */
+        private $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
     /**
      * Get id
      *
@@ -54,5 +65,39 @@
     public function getDesignation()
     {
         return $this->designation;
+    }
+
+    /**
+     * Add product
+     *
+     * @param \AppBundle\Entity\Product $product
+     *
+     * @return Category
+     */
+    public function addProduct(\AppBundle\Entity\Product $product)
+    {
+        $this->products[] = $product;
+
+        return $this;
+    }
+
+    /**
+     * Remove product
+     *
+     * @param \AppBundle\Entity\Product $product
+     */
+    public function removeProduct(\AppBundle\Entity\Product $product)
+    {
+        $this->products->removeElement($product);
+    }
+
+    /**
+     * Get products
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getProducts()
+    {
+        return $this->products;
     }
 }
