8c37d159a9505a1a5ff7fdbcf3850379c656bd6c
Maks <maxence.derous@gmail.com>
2017-05-05 10:44:47 +0200

    refactored product form to a FormType class

---
+++ src/AppBundle/Form/ProductType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+class ProductType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('reference', TextType::class)
+            ->add('price', NumberType::class)
+            ->add('category', EntityType::class, 
+                [
+                    'class' => 'AppBundle:Category',
+                    'choice_label' => 'designation',
+                ]
+            )
+            ->add('save', SubmitType::class, 
+                [
+                'label' => 'Save',
+                    'attr' => [
+                        'class' => 'btn btn-primary'
+                    ]
+                ]
+            );
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Product'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_product';
+    }
+
+
+}
