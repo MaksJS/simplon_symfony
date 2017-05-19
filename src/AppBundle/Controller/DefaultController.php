4b8544ec72fe9f821571318148f86aef47f73d3d
Maks <maxence.derous@gmail.com>
2017-05-19 11:41:21 +0200

    added HTTP_BASIC security example php bin/console security:encode-password to generate an encoded password

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -14,5 +15,13 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         return $this->redirectToRoute('client_index');
+    }
+    
+    /**
+     * @Route("/admin")
+     */
+    public function adminAction()
+    {
+        return new Response('<html><body>Admin page!</body></html>');
     }
 }
