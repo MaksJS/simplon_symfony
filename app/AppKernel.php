d9fa54482c87aadca0229807df5836fba8da72a3
Maks <maxence.derous@gmail.com>
2017-05-19 14:03:39 +0200

    added friendsofsymfony/user-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Simplon\BlogBundle\SimplonBlogBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
